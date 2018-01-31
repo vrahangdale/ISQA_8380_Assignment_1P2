e9dab3134009dd0c54eea4defc2f54780c27932d
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2018-01-31 00:03:50 -0600

    added deployment flies

--- efsblog/efsblog/settings.py
+++ efsblog/efsblog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,10 +25,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fe$5sr_0e#0sctexf*%0#!bxo1n-(#@ozs*4eaic4rmtgt%fqi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # Application definition
 
@@ -79,9 +89,14 @@ WSGI_APPLICATION = 'efsblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dcnb455hmdhpp5',
+        'USER': 'lejzslmifkukyy',
+        'PASSWORD': 'ba96fe7ef21bcec8f9273f692d8f1728011e36ffbbcbd681a631870228ff31c3',
+        'HOST': 'ec2-23-23-92-179.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
+
 }
 
 
@@ -121,4 +136,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
